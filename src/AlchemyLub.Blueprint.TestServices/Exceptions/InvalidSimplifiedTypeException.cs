cf948865da81bbe61a42a5c272241c8cd8944fb5
Vladislav Rudakov <vladislav.rudakoff@gmail.com>
2024-06-01 22:31:40 +0300

    feat: structural comparison move into https://github.com/VladislavRudakoff/StructuralComparison

--- src/AlchemyLub.Blueprint.TestServices/Exceptions/InvalidSimplifiedTypeException.cs
+++
@@ -1,6 +0,0 @@
-namespace AlchemyLub.Blueprint.TestServices.Exceptions;
-
-/// <summary>
-/// Исключение для типа не относящегося ни к одной группе
-/// </summary>
-public class InvalidSimplifiedTypeException(Type type) : Exception($"{type.FullName} относится к неизвестному типу данных");
