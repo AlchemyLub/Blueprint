d58b666b23c5d7a5400c083aae0c40f7cdfb297a
Vladislav Rudakov <vladislav.rudakoff@gmail.com>
2024-07-06 23:24:46 +0300

    feat: init generator

---
+++ src/AlchemyLub.Blueprint.Generator/DecoratorGenerator.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace AlchemyLub.Blueprint.Generator;
+
+[Generator]
+public sealed class DecoratorGenerator : IIncrementalGenerator
+{
+    private const string DecoratorAttributeFileName = "DecoratorAttribute.g.cs";
+
+    /// <inheritdoc />
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        if (!Debugger.IsAttached)
+        {
+            Debugger.Launch();
+        }
+
+        context.RegisterPostInitializationOutput(postInitContext => postInitContext.AddSource(
+            DecoratorAttributeFileName,
+            SourceText.From(RawAttributes.DecoratorAttributes, Encoding.UTF8)));
+
+        IncrementalValuesProvider<INamedTypeSymbol> pipeline = context.SyntaxProvider.ForAttributeWithMetadataName(
+            "DecoratorGenerator.Attributes.DecorateAttribute`1",
+            (node, _) => node is ClassDeclarationSyntax classDeclaration
+                         && classDeclaration.Members.OfType<MethodDeclarationSyntax>().Any()
+                         && classDeclaration.BaseList is { Types.Count: > 0},
+            (attributeContext, _) =>
+            {
+                if (attributeContext.TargetSymbol is not INamedTypeSymbol classSymbol)
+                {
+                    return null;
+                }
+
+                return classSymbol;
+            })
+            .Where(t => t is not null)!;
+
+        context.RegisterSourceOutput(pipeline, Execute);
+    }
+
+    private void TestClass(INamedTypeSymbol classSymbol)
+    {
+
+    }
+
+    private static void Execute(SourceProductionContext context, INamedTypeSymbol classSymbol)
+    {
+        if (classSymbol.IsAbstract)
+        {
+            return;
+        }
+
+        string code = GenerateCode();
+
+        context.AddSource("DecoratorExtensions.g", code);
+    }
+
+    private static string GenerateCode() => string.Empty;
+}
---
+++ src/AlchemyLub.Blueprint.Generator/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using System.Diagnostics;
+global using System.Text;
+global using AlchemyLub.Blueprint.Generator.RawConstantFiles;
+global using Microsoft.CodeAnalysis;
+global using Microsoft.CodeAnalysis.CSharp.Syntax;
+global using Microsoft.CodeAnalysis.Text;
